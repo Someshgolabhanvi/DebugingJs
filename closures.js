c9c3c9bb7880ac7290a053867b6816a706734e75
Someshgolabhanvi <someshsg02@gmail.com>
2024-10-21 21:42:19 +0530

    Clouser

--- closures.js
+++ closures.js
@@ -1,11 +1,22 @@
-function x(){
-    var a = 7;
-    function y(){
-        console.log(a);
-    }
-    return y;
-}
-var z = x();
-console.log(z);
+// function x(){
+//     var a = 7;
+//     function y(){
+//         console.log(a);
+//     }
+//     return y;
+// }
+// var z = x();
+// console.log(z);
 
-z();
+// z();
+
+function x(){
+    var i =1;
+    setTimeout(function(){
+        console.log(i);
+        
+    },5000)
+    console.log("Hi");
+    
+}
+x();
