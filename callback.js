9985886625bcd2008549e001fdc83d9fa03e466e
Someshgolabhanvi <someshsg02@gmail.com>
2024-10-25 21:58:55 +0530

    callback functions

--- callback.js
+++ callback.js
@@ -13,17 +13,32 @@
 // console.log("End");
 
 
-console.log("start");
-setTimeout(function cbT(){
-    console.log("CB SetTimeout");
+// console.log("start");
+// setTimeout(function cbT(){
+//     console.log("CB SetTimeout");
     
-},5000);
-fetch("https://www.netflix.com/in/").then(function cbF(){
-    console.log("CB Netflix");
-});
+// },5000);
+// fetch("https://www.netflix.com/in/").then(function cbF(){
+//     console.log("CB Netflix");
+// });
+// console.log("End");
+
+//Trust Issues with setTimeout()
+//Concercy model
+
+
+console.log("Start");
+setTimeout(function ab(){
+    console.log("Callback");
+},5000)
 console.log("End");
 
+let startDate = new Date().getTime();
+let endDate = startDate;
+while (endDate<=startDate + 10000) {
+    endDate = new Date().getTime();
+}
 
-
+console.log("While expires");
 
 
