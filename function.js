e439c54e4dad83233b70aea921ae46503464b518
Someshgolabhanvi <someshsg02@gmail.com>
2024-10-26 21:48:18 +0530

    callback functions

--- function.js
+++ function.js
@@ -1,13 +1,13 @@
 //Function Statement or function Declaration
-function a(){
-    console.log("a called");
-}
-a();
-//Function Expression
-var b = function () {
-    console.log("b called");
-}
-b();
+// function a(){
+//     console.log("a called");
+// }
+// a();
+// //Function Expression
+// var b = function () {
+//     console.log("b called");
+// }
+// b();
 
 //Anonymous Function
 // function (){
@@ -28,3 +28,29 @@ b();
 //First Class Function
 
 //Arrow Function
+
+
+
+//Higher Order Functions
+
+
+const radius = [3,1,2,4];
+
+const calculateArea = function(radius){
+    const output = [];
+    for(let i = 0;i<radius.length;i++){
+        output.push(Math.PI * radius[i] * radius[i]);
+    }
+    return output;
+}
+console.log(calculateArea(radius));
+
+
+const calculateCircumference = function(radius){
+    const output = [];
+    for(let i = 0;i<radius.length;i++){
+        output.push(2*Math.PI * radius[i]);
+    }
+    return output;
+}
+console.log(calculateCircumference(radius));
